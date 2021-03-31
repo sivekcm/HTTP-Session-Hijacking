348145ab9a4b8df4b90529b2b12e701394f7fd84
Christopher Sivek <sivekcm@vcu.edu>
2021-03-31 16:52:10 -0400

    new pages for account, cart, shop

---
+++ account.php
@@ -0,0 +1,32 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="/css/uikit.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <nav class="uk-navbar-container uk-navbar">
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+          <li class="uk-parent"><a href="/home.php">Home</a></li>
+          <li class="uk-parent"><a href="/shop.php">Shop</a></li>
+          <li class="uk-active"><a href="/account.php">Account</a></li>
+        </ul>
+    </div>
+    <div class="uk-navbar-right">
+      <ul class="uk-navbar-nav">
+        <li class="uk-parent">
+          <a href="/cart.php">
+            <img height="30" width="30" src="https://img.icons8.com/ios/50/000000/shopping-cart-loaded--v2.png"/>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  </body>
+</html>
--- cart.php
+++ cart.php
@@ -10,23 +10,23 @@
   </head>
   <body>
     <nav class="uk-navbar-container uk-navbar">
-      <div class="uk-navbar-left">
-          <ul class="uk-navbar-nav">
-            <li class="uk-active"><a href="/home.php">Home</a></li>
-            <li class="uk-parent"><a href="/shop.php">Shop</a></li>
-            <li class="uk-parent"><a href="/account.php">Account</a></li>
-          </ul>
-      </div>
-      <div class="uk-navbar-right">
+    <div class="uk-navbar-left">
         <ul class="uk-navbar-nav">
-          <li class="uk-parent">
-            <a href="/cart.php">
-              <img height="30" width="30" src="https://img.icons8.com/ios/50/000000/shopping-cart-loaded--v2.png"/>
-            </a>
-          </li>
+          <li class="uk-parent"><a href="/home.php">Home</a></li>
+          <li class="uk-parent"><a href="/shop.php">Shop</a></li>
+          <li class="uk-parent"><a href="/account.php">Account</a></li>
         </ul>
-      </div>
-    </nav>
+    </div>
+    <div class="uk-navbar-right">
+      <ul class="uk-navbar-nav">
+        <li class="uk-active">
+          <a href="/cart.php">
+            <img width="30" height="30" src="https://img.icons8.com/ios/50/000000/shopping-cart-loaded--v2.png"/>
+          </a>
+        </li>
+      </ul>
+    </div>
+</nav>
 
   <div class="uk-position-center">
     <h2 class="uk-heading-medium">Welcome!</h2>
